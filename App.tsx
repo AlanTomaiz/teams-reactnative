931449f322c08e9f4a61badbe75fc7500de4d8dd
Alanderson Tomaiz <alan_tomaiz@hotmail.com>
2025-01-03 19:10:32 -0300

    styled components

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import Groups from '@screens/groups'
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto'
 import { StatusBar } from 'expo-status-bar'
+import { ThemeProvider } from 'styled-components/native'
+
+import { Loader } from '@components/loader'
+import Groups from '@screens/groups'
+import theme from './src/theme'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
-    <>
-      <Groups />
-      <StatusBar style="auto" translucent />
-    </>
+    <ThemeProvider theme={theme}>
+      <StatusBar style="light" translucent />
+      {!fontsLoaded ? <Loader /> : <Groups />}
+    </ThemeProvider>
   )
 }
